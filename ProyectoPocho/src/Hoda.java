6cd42a42cbfbba2587420f025886d22266378448
Carlos Peña <carlos.pena@opendeusto.es>
2023-09-22 09:29:57 +0200

    Print Message

--- ProyectoPocho/src/Hoda.java
+++ ProyectoPocho/src/Hoda.java
@@ -1,6 +1,6 @@
 
 public class Hoda {
-public static void main(String[] args) {
-	System.out.println("Hola Mundo!");
-}
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
+	}
 }
